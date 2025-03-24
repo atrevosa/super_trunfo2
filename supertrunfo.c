73d286f3a99d97f444d01a1585d406ce03509506
atrevosa <ju.leonel13@gmail.com>
2025-03-24 00:52:46 +0000

    fodase esses erros

--- supertrunfo.c
+++ supertrunfo.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 
+//main
+
 int main() {
     char estado1, estado2;
     char codigo1[4], codigo2[4];
     char nomeCidade1[50], nomeCidade2[50];
     int populacao1, populacao2;
-    float area1, area2;
-    float pib1, pib2;
-    int pontosTuristicos1, pontosTuristicos2;
+    float area1, area2, pib1, pib2;
+    int pontosturisticos1, pontosturisticos2;
 
-    // Entrada de dados da primeira carta
+//organização apresentação do programa
+        printf("\n");
+
+    printf("SUPER TRUNFO! \n");
+    printf("Carregando...... \n");
+
+        printf("\n");
+        printf("\n");
+
+//primeira carta (dados)
+
     printf("Digite os dados da primeira carta:\n");
     printf("Estado (A-H): ");
     scanf(" %c", &estado1);
@@ -19,14 +30,16 @@ int main() {
     scanf(" %s", &nomeCidade1);
     printf("População: ");
     scanf(" %d", &populacao1);
-    printf("Área (em km²): ");
+    printf("Área (em km²) somente números: ");
     scanf(" %f", &area1);
-    printf("PIB (em bilhões de reais): ");
+    printf("PIB (em bilhões de reais) somente números: ");
     scanf(" %f", &pib1);
     printf("Número de Pontos Turísticos: ");
-    scanf(" %d", &pontosTuristicos1);
+    scanf(" %d", &pontosturisticos1);   
 
-    // Entrada de dados da segunda carta
+ //segunda carta (entrada de dados)
+        printf("\n");
+
     printf("\nDigite os dados da segunda carta:\n");
     printf("Estado (A-H): ");
     scanf(" %c", &estado2);
@@ -36,14 +49,23 @@ int main() {
     scanf(" %s", &nomeCidade2);
     printf("População: ");
     scanf(" %d", &populacao2);
-    printf("Área (em km²): ");
+    printf("Área (em km²) somente números: ");
     scanf(" %f", &area2);
-    printf("PIB (em bilhões de reais): ");
+    printf("PIB (em bilhões de reais) somente números: ");
     scanf(" %f", &pib2);
     printf("Número de Pontos Turísticos: ");
-    scanf(" %d", &pontosTuristicos2);
+    scanf(" %d", &pontosturisticos2);
 
-    // Exibição dos dados cadastrados
+        printf("\n");
+        printf("\n");
+
+
+//exibição dos dados (resultados)
+
+    printf("Carregando...... \n");
+    printf("Confira os dados das cartas! \n");
+        printf("\n");
+
     printf("\nCarta 1:\n");
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
@@ -51,7 +73,7 @@ int main() {
     printf("População: %d\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
-    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+    printf("Número de Pontos Turísticos: %d\n", pontosturisticos1);
 
     printf("\nCarta 2:\n");
     printf("Estado: %c\n", estado2);
@@ -60,7 +82,7 @@ int main() {
     printf("População: %d\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
-    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+    printf("Número de Pontos Turísticos: %d\n", pontosturisticos2);
 
     return 0;
 }
